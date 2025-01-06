63b6455dc87508d50b2dcc7b8cf981bab814334d
Pedro Shin <86082102+pedromshin@users.noreply.github.com>
2025-01-05 23:55:56 -0300

    Initial commit

---
+++ apps/nextjs/src/app/dashboard/page.tsx
@@ -0,0 +1,50 @@
+import { Suspense } from "react";
+
+import {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@acme/ui/breadcrumb";
+
+import { api, HydrateClient } from "~/trpc/server";
+import { AuthShowcase } from "./_components/auth-showcase";
+import {
+  CreatePostForm,
+  PostCardSkeleton,
+  PostList,
+} from "./_components/posts";
+
+import { ScrollArea } from "@acme/ui/scroll-area"
+
+export const runtime = "edge";
+
+export default function HomePage() {
+  // You can await this here if you don't want to show Suspense fallback below
+  void api.post.all.prefetch();
+
+  return (
+    <HydrateClient>
+      <div className="flex-1 space-y-4 p-4 pt-2 md:p-6 md:pt-4 lg:p-8 lg:pt-6">
+        <CreatePostForm />
+
+        <ScrollArea className="w-full overflow-y-auto h-[65dvh]">
+          <Suspense
+            fallback={
+              <div className="flex w-full flex-col gap-4">
+                <PostCardSkeleton />
+                <PostCardSkeleton />
+                <PostCardSkeleton />
+              </div>
+            }
+          >
+            <PostList />
+          </Suspense>
+        </ScrollArea>
+      </div>
+    </HydrateClient>
+  );
+}
